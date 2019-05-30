b21a7421abd584f759e233eb8cdbe67588399520
i_hemingway <Ian.Hemingway@mindgeek.com>
2019-05-30 15:18:02 -0400

    init

---
+++ vars/pullCicada.groovy
@@ -0,0 +1,13 @@
+#!/usr/bin/env groovy
+
+
+def call(String cicadaurl = 'https://stash.mgcorp.co/scm/lt/cicada.git') {
+    checkout(
+        [$class: 'GitSCM',
+        branches: [[name: "*/master"]],
+        doGenerateSubmoduleConfigurations: false,
+        extensions: [[$class: 'CloneOption', depth: 1, noTags: true, reference: '', shallow: true]],
+        userRemoteConfigs: [[credentialsId: 'StashKey', url: "${cicadaurl}"]]
+        ]
+    )
+}
