5be19d77b5dbc5795558f5f8acd4555ad26645e4
i_hemingway <Ian.Hemingway@mindgeek.com>
2019-06-03 12:05:27 -0400

    fix

--- vars/getNotifyEmails.groovy
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env groovy
-
-def call(Map args) {
-    def yaml = readYaml file: "${args.config}"
-    def emails = 
-}
--- vars/pullManifests.groovy
+++ vars/pullManifests.groovy
@@ -1,12 +1,12 @@
 #!/usr/bin/env groovy
 
 
-def call(Map args, String MANIFESTS_URL = 'ssh://git@stash.mgcorp.co:7999/lt/deployment-manifests.git') {
+def call(String MANIFESTS_URL = 'ssh://git@stash.mgcorp.co:7999/lt/deployment-manifests.git') {
     checkout(
         [$class: 'GitSCM',
         branches: [[name: "*/master"]],
         doGenerateSubmoduleConfigurations: false,
-        extensions: [[$class: 'CloneOption', depth: 1, noTags: true, reference: '', shallow: true]],
+        extensions: [[$class: 'CloneOption', depth: 1, noTags: true, , shallow: true]],
         userRemoteConfigs: [[credentialsId: 'StashKey', url: "${MANIFESTS_URL}"]]
         ]
     )
