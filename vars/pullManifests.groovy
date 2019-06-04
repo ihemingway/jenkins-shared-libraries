47fc7f90391946b7dad0d70a74e9a7d281834892
i_hemingway <Ian.Hemingway@mindgeek.com>
2019-06-04 12:39:52 -0400

    var update

--- vars/pullManifests.groovy
+++ vars/pullManifests.groovy
@@ -1,13 +1,13 @@
 #!/usr/bin/env groovy
 
 
-def call(String MANIFESTS_URL = 'ssh://git@stash.mgcorp.co:7999/lt/deployment-manifests.git') {
+def call(String manifestsurl = 'ssh://git@stash.mgcorp.co:7999/lt/deployment-manifests.git') {
     checkout(
         [$class: 'GitSCM',
         branches: [[name: "*/master"]],
         doGenerateSubmoduleConfigurations: false,
         extensions: [[$class: 'CloneOption', depth: 1, noTags: true, , shallow: true]],
-        userRemoteConfigs: [[credentialsId: 'StashKey', url: "${MANIFESTS_URL}"]]
+        userRemoteConfigs: [[credentialsId: 'StashKey', url: "${manifestsurl}"]]
         ]
     )
 }
