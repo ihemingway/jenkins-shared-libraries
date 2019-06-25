b525b2d278f41b083af3cb2e5a2630d55ef58e82
i_hemingway <Ian.Hemingway@mindgeek.com>
2019-06-25 14:22:41 -0400

    fv

--- vars/vaultPull.groovy
+++ vars/vaultPull.groovy
@@ -3,12 +3,12 @@
 def call(Map args) {
     def exists = fileExists '/usr/sbin/vault'
     if (exists) {
+        echo "Vault binary found."
+    } else {
         sh '''
             wget https://repository.mgcorp.co/artifactory/devops-misc/vault_1.0.2_linux_amd64.zip
             unzip -d /usr/sbin vault_1.0.2_linux_amd64.zip
         '''
-    } else {
-        echo "Vault binary found."
     }
     sh '''
         set -x
