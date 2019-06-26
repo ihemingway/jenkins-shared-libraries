656f5a018f240b910910d073b7b40ce8e188f870
i_hemingway <Ian.Hemingway@mindgeek.com>
2019-06-26 11:38:16 -0400

    wfeg

--- vars/vaultPull.groovy
+++ vars/vaultPull.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-def call(String vaulttoken, String key, String path, String vaultzipurl = "https://repository.mgcorp.co/artifactory/devops-misc/vault_1.0.2_linux_amd64.zip") {
+def call(Map args, String vaultzipurl = "https://repository.mgcorp.co/artifactory/devops-misc/vault_1.0.2_linux_amd64.zip") {
     def exists = fileExists '/usr/sbin/vault'
     if (exists) {
         echo "Vault binary found."
@@ -10,7 +10,7 @@ def call(String vaulttoken, String key, String path, String vaultzipurl = "https
             unzip vault.zip -d /usr/sbin
         """
     }
-    sh(returnStdout: true, script:"vault login ${vaulttoken} > /dev/null 2>&1")
-    result = sh(returnStdout: true, script:"vault kv get -field=${key} secret/${path}")
+    sh(returnStdout: true, script:"vault login ${args.vaulttoken} > /dev/null 2>&1")
+    result = sh(returnStdout: true, script:"vault kv get -field=${args.key} secret/${args.path}")
     return result
 }
