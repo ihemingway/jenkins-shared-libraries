546fca2ec486b8a090ba6d011202e9173f2d970f
i_hemingway <Ian.Hemingway@mindgeek.com>
2019-07-04 12:40:44 -0400

    jk

--- vars/notifyDevOps.groovy
+++ vars/notifyDevOps.groovy
@@ -18,7 +18,7 @@ def call(Map args) {
         webhookUrl: "https://outlook.office.com/webhook/ea64b24e-111b-4b31-b1c4-ed67ce8c9ef4@8901d9c6-8b0c-4459-8f7a-df56f23ef9f9/JenkinsCI/24f71ab16f7f4f63a08b76b45c5ab4e0/acc9e43c-a995-47e2-b550-5a5aa2568437",
         status: "${args.status}"
         )
-    if(sendemail = true) {
+    if(sendemail) {
     emailext(
         body: """${env.JOB_NAME} - Build # ${env.BUILD_NUMBER} - DONE: ${args.status}
 
