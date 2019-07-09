14c5d4835b69a656ee5211f49f7051c39d6524b3
i_hemingway <Ian.Hemingway@mindgeek.com>
2019-07-09 15:07:06 -0400

    ddf

--- vars/pullCode.groovy
+++ vars/pullCode.groovy
@@ -1,9 +1,9 @@
 #!/usr/bin/env groovy
 
 
-def call(Map args) {
-    def repo = args.repo ?: env.CODE_URL
-    def branch = args.branch ?: env.BRANCH
+def call(String repo = env.CODE_URL, String branch = env.BRANCH) {
+    /*def repo = args.repo ?: env.CODE_URL
+    def branch = args.branch ?: env.BRANCH*/
     checkout(
         [$class: 'GitSCM',
         branches: [[name: "${branch}"]],
