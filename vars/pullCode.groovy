c45e439bfeaf57f75b478c10a7abdb2c5bc68b12
i_hemingway <Ian.Hemingway@mindgeek.com>
2019-07-09 15:09:02 -0400

    df

--- vars/pullCode.groovy
+++ vars/pullCode.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 
 
-def call(String repo = env.CODE_URL, String branch = env.BRANCH) {
+def call(repo = env.CODE_URL, branch = env.BRANCH) {
     /*def repo = args.repo ?: env.CODE_URL
     def branch = args.branch ?: env.BRANCH*/
     checkout(
