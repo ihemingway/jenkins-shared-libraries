924cbd40dcccf976c6690fb8554cbcff40f982cf
i_hemingway <Ian.Hemingway@mindgeek.com>
2019-07-09 15:18:57 -0400

    map

--- vars/pullCode.groovy
+++ vars/pullCode.groovy
@@ -1,15 +1,15 @@
 #!/usr/bin/env groovy
 
 
-def call(repo = env.CODE_URL, branch = env.BRANCH) {
+def call(Map args) {
     /*def repo = args.repo ?: env.CODE_URL
     def branch = args.branch ?: env.BRANCH*/
     checkout(
         [$class: 'GitSCM',
-        branches: [[name: "${branch}"]],
+        branches: [[name: "${args.branch}"]],
         doGenerateSubmoduleConfigurations: false,
         extensions: [[$class: 'CloneOption', noTags: false,  shallow: false]],
-        userRemoteConfigs: [[credentialsId: 'StashKey', url: "${repo}"]]
+        userRemoteConfigs: [[credentialsId: 'StashKey', url: "${args.repo}"]]
         ]
     )
 }
