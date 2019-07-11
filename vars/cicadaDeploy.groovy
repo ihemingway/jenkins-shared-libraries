d1d3ca9eb3dfe100d90dec07c7dc1d849217ee75
i_hemingway <Ian.Hemingway@mindgeek.com>
2019-07-11 15:15:43 -0400

    debugging toggling

--- vars/cicadaDeploy.groovy
+++ vars/cicadaDeploy.groovy
@@ -8,9 +8,14 @@ cicadaDeploy()
 */
 
 def call(Map args) {
+    def debug = args.debug ?: false
+    def flag = ""
+    if(debug) {
+        flag = "-d"
+    }
     withCredentials([string(credentialsId: env.VAULT_TOKEN, variable: 'VAULT_TOKEN')]) {
         sh (
-            """cd ${env.WORKSPACE} && PYTHONIOENCODING=utf8 mgdeploy -c ${env.WORKSPACE}/deployment-manifests/${env.PROJPROD}/deployconfig.yaml -e "${env.ENVIRONMENT}" -p `ls -1t atomic* | head -n 1` -d"""
+            """cd ${env.WORKSPACE} && PYTHONIOENCODING=utf8 mgdeploy -c ${env.WORKSPACE}/deployment-manifests/${env.PROJPROD}/deployconfig.yaml -e "${env.ENVIRONMENT}" -p `ls -1t atomic* | head -n 1` ${flag}"""
         )
     }
 }
--- vars/cicadaPackage.groovy
+++ vars/cicadaPackage.groovy
@@ -8,7 +8,12 @@ cicadaPackage()
 */
 
 def call(Map args) {
+    def debug = args.debug ?: false
+    def flag = ""
+    if(debug) {
+        flag = "-d"
+    }
     sh (
-        """cd ${env.WORKSPACE} && PYTHONIOENCODING=utf8 mgpackage -c ${env.WORKSPACE}/deployment-manifests/${env.PROJPROD}/deployconfig.yaml -e "${env.ENVIRONMENT}" -d"""
+        """cd ${env.WORKSPACE} && PYTHONIOENCODING=utf8 mgpackage -c ${env.WORKSPACE}/deployment-manifests/${env.PROJPROD}/deployconfig.yaml -e "${env.ENVIRONMENT}" ${flag}"""
     )
 }
