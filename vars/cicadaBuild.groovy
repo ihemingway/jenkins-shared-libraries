cd552c3476059fa3894cee880485061efe30bc21
i_hemingway <Ian.Hemingway@mindgeek.com>
2019-07-11 16:34:33 -0400

    disable debug

--- vars/cicadaBuild.groovy
+++ vars/cicadaBuild.groovy
@@ -8,11 +8,11 @@ cicadaBuild(
 */
 
 def call(Map args) {
-    def debug = args.debug ?: false
+    //def debug = args.debug ?: false
     def flag = ""
-    if(debug) {
+    /*if(debug) {
         flag = "-d"
-    }
+    }*/
     sh (
         """PYTHONIOENCODING=utf8 mgbuild -c ${env.WORKSPACE}/deployment-manifests/${env.PROJPROD}/deployconfig.yaml -e "${env.ENVIRONMENT}" ${flag}"""
     )
--- vars/cicadaDeploy.groovy
+++ vars/cicadaDeploy.groovy
@@ -8,11 +8,11 @@ cicadaDeploy(
 */
 
 def call(Map args) {
-    def debug = args.debug ?: false
+    //def debug = args.debug ?: false
     def flag = ""
-    if(debug) {
+    /*if(debug) {
         flag = "-d"
-    }
+    }*/
     withCredentials([string(credentialsId: env.VAULT_TOKEN, variable: 'VAULT_TOKEN')]) {
         sh (
             """cd ${env.WORKSPACE} && PYTHONIOENCODING=utf8 mgdeploy -c ${env.WORKSPACE}/deployment-manifests/${env.PROJPROD}/deployconfig.yaml -e "${env.ENVIRONMENT}" -p `ls -1t atomic* | head -n 1` ${flag}"""
