330a8f71d7c4e37c565c262ea0797a40fa70d316
i_hemingway <Ian.Hemingway@mindgeek.com>
2019-07-11 14:51:19 -0400

    debug flag

--- vars/cicadaBuild.groovy
+++ vars/cicadaBuild.groovy
@@ -7,7 +7,12 @@ cicadaBuild()
 */
 
 def call(Map args) {
+    def debug = args.debug ?: false
+    def flag = ""
+    if(debug) {
+        flag = "-d"
+    }
     sh (
-        """PYTHONIOENCODING=utf8 mgbuild -c ${env.WORKSPACE}/deployment-manifests/${env.PROJPROD}/deployconfig.yaml -e "${env.ENVIRONMENT}" -d"""
+        """PYTHONIOENCODING=utf8 mgbuild -c ${env.WORKSPACE}/deployment-manifests/${env.PROJPROD}/deployconfig.yaml -e "${env.ENVIRONMENT}" ${flag}"""
     )
 }
