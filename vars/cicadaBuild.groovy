20fe23da3338d9512fa57f7ed22cf9c2ba102b76
i_hemingway <Ian.Hemingway@mindgeek.com>
2019-07-11 16:50:25 -0400

    test

--- vars/cicadaBuild.groovy
+++ vars/cicadaBuild.groovy
@@ -13,6 +13,9 @@ def call(Map args) {
     /*if(debug) {
         flag = "-d"
     }*/
+    if (?.args.?debug == true) {
+        flag = "-d"
+    }
     sh (
         """PYTHONIOENCODING=utf8 mgbuild -c ${env.WORKSPACE}/deployment-manifests/${env.PROJPROD}/deployconfig.yaml -e "${env.ENVIRONMENT}" ${flag}"""
     )
--- vars/cicadaPackage.groovy
+++ vars/cicadaPackage.groovy
@@ -12,6 +12,10 @@ def call(Map args) {
     def flag = ""
     /*if(debug) {
         flag = "-d"
+    }
+    
+    if (?.args.?debug == true) {
+        flag = "-d"
     }*/
     sh (
         """cd ${env.WORKSPACE} && PYTHONIOENCODING=utf8 mgpackage -c ${env.WORKSPACE}/deployment-manifests/${env.PROJPROD}/deployconfig.yaml -e "${env.ENVIRONMENT}" ${flag}"""
