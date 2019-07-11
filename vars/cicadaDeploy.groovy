9fe6df78c59445e6507b7842e02f399ae81da7e6
i_hemingway <Ian.Hemingway@mindgeek.com>
2019-07-11 16:59:56 -0400

    debug patch

--- vars/cicadaDeploy.groovy
+++ vars/cicadaDeploy.groovy
@@ -8,11 +8,10 @@ cicadaDeploy(
 */
 
 def call(Map args) {
-    //def debug = args.debug ?: false
     def flag = ""
-    /*if(debug) {
+    if (args?.debug == true) {
         flag = "-d"
-    }*/
+    }
     withCredentials([string(credentialsId: env.VAULT_TOKEN, variable: 'VAULT_TOKEN')]) {
         sh (
             """cd ${env.WORKSPACE} && PYTHONIOENCODING=utf8 mgdeploy -c ${env.WORKSPACE}/deployment-manifests/${env.PROJPROD}/deployconfig.yaml -e "${env.ENVIRONMENT}" -p `ls -1t atomic* | head -n 1` ${flag}"""
--- vars/cicadaPackage.groovy
+++ vars/cicadaPackage.groovy
@@ -8,15 +8,10 @@ cicadaPackage(
 */
 
 def call(Map args) {
-    //def debug = args.debug ?: false
     def flag = ""
-    /*if(debug) {
+    if (args?.debug == true) {
         flag = "-d"
     }
-    
-    if (?.args.?debug == true) {
-        flag = "-d"
-    }*/
     sh (
         """cd ${env.WORKSPACE} && PYTHONIOENCODING=utf8 mgpackage -c ${env.WORKSPACE}/deployment-manifests/${env.PROJPROD}/deployconfig.yaml -e "${env.ENVIRONMENT}" ${flag}"""
     )
